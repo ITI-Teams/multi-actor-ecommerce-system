10e129f9c3d1fe1d90e699696c03def0297e0575
Ahmed Alaa <ahmed.alla56756@gmail.com>
2025-08-14 00:49:07 +0300

    making products page and rendering products with pagination and editing test.js with the product view and enhance the UX of it

--- assets/js/test.js
+++ assets/js/test.js
@@ -17,7 +17,7 @@ window.addEventListener("load", () => {
     let index = 0;
     let index2 = 0;
 
-    productsArray.forEach(({ thumbnail, title, price, category, rating, images }) => {
+    productsArray.forEach(({ thumbnail, title, price, category, rating, images, id }) => {
       if (category.includes("men") || category.includes("women")) {
         // if the category is men or women
         if (index < 10) {
@@ -26,38 +26,43 @@ window.addEventListener("load", () => {
           // adding product to new arrivals section
           const newArrivlesWrapper =
             document.getElementById("newArrivlesWrapper");
+            if (newArrivlesWrapper) {
           newArrivlesWrapper.innerHTML += `<div>
           <div class="img-container position-relative">
+            <a href="/pages/onepage-product.html?product=${id}" class="text-dark link-offset-1-hover text-uppercase" >
               <img class="prodcut-img bg-secondary-subtle mb-2 primary-img" 
               src="${thumbnail}" 
               alt="${title}"
               data-original="${thumbnail}"
-              data-hover="${images[1]}"
+              data-hover="${images[1] || images[0]}"
               onmouseover="this.src=this.dataset.hover" onmouseout="this.src=this.dataset.original">
             </div>
-          <a href="/" class="text-dark link-offset-1-hover text-uppercase" >${title}</a>
+            ${title}</a>
           <p>${price}$</p>
           </div>`;
+        }
         }
 
         // adding products to best seller section
         if (rating > 4 && index2 < 12) { // getting the products with rating > 4 and 12 products only
           ++index2
           const bestSellerProductsWrapper = document.getElementById("bestSellerProductsWrapper");
+          if (bestSellerProductsWrapper) {
           bestSellerProductsWrapper.innerHTML += `
           <div class="d-flex flex-column mt-3">
-            <div class="img-container position-relative">
+          <div class="img-container position-relative">
+          <a href="/pages/onepage-product.html?product=${id}" class=" text-dark link-offset-1-hover text-uppercase" >
               <img class="prodcut-img bg-secondary-subtle mb-2 primary-img" 
               src="${thumbnail}" 
               alt="${title}"
               data-original="${thumbnail}"
-              data-hover="${images[1]}"
+              data-hover="${images[1] || images[0]}"
               onmouseover="this.src=this.dataset.hover" onmouseout="this.src=this.dataset.original">
             </div>
-            <a href="/" class=" text-dark link-offset-1-hover text-uppercase" >${title}</a>
+            ${title}</a>
             <p>${price}$</p>
           </div>`;
-        }
+        }}
       }
     });
   }
